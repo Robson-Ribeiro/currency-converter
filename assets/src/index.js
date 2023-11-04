368192986ced2a1bd1c19a3d2f27716ecac840e2
Robson <robsonribeirorra@gmail.com>
2023-11-04 15:27:47 -0300

    Adding the inversor functionality

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,6 @@
 const fromCur = document.querySelector(".from select");
 const toCur = document.querySelector(".to select");
+const inversor = document.querySelector(".exchangeConfig i");
 
 [fromCur, toCur].forEach((select, i) => {
     for(let curCode in Country_List) {
@@ -9,5 +10,13 @@ const toCur = document.querySelector(".to select");
     select.addEventListener("change", () => {
         const imgElement = select.parentElement.querySelector("img");
         imgElement.src = `https://flagcdn.com/48x36/${Country_List[select.value].toLowerCase()}.png`;
+    });
+});
+
+inversor.addEventListener("click", () => {
+    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
+    [fromCur, toCur].forEach( select => {
+        const imgElement = select.parentElement.querySelector("img");
+        imgElement.src = `https://flagcdn.com/48x36/${Country_List[select.value].toLowerCase()}.png`; 
     });
 });
