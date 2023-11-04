6b66badbf9d2fe56a0b06a234326964615c8b826
Robson <robsonribeirorra@gmail.com>
2023-11-04 18:00:29 -0300

    Creating the currency conversor and finishing the MVP

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,6 +1,11 @@
 const fromCur = document.querySelector(".from select");
 const toCur = document.querySelector(".to select");
 const inversor = document.querySelector(".exchangeConfig i");
+const amountInput = document.querySelector(".exchangeValue input");
+const response = document.querySelector(".response p");
+const button = document.querySelector(".convertButton");
+
+const KEY = " ";
 
 [fromCur, toCur].forEach((select, i) => {
     for(let curCode in Country_List) {
@@ -13,10 +18,38 @@ const inversor = document.querySelector(".exchangeConfig i");
     });
 });
 
+const conversor = async () => {
+    amountInput.value = amountInput.value || 1;
+    response.innerText = "Processing your value...";
+
+    try {
+        const res = await fetch(`https://v6.exchangerate-api.com/v6/${KEY}/latest/${fromCur.value}`);
+        const result = await res.json();
+
+        const currencyRate = result.conversion_rates[toCur.value];
+        const convertedValue = amountInput.value * currencyRate;
+
+        response.innerText = `${amountInput.value} ${fromCur.value} = ${convertedValue.toFixed(2)} ${toCur.value}`;
+    } catch (error) {
+        response.innerText = "Sorry, We're not able to make this conversion!";
+    }
+}
+
 inversor.addEventListener("click", () => {
     [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
     [fromCur, toCur].forEach( select => {
         const imgElement = select.parentElement.querySelector("img");
         imgElement.src = `https://flagcdn.com/48x36/${Country_List[select.value].toLowerCase()}.png`; 
     });
+    conversor();
 });
+
+button.addEventListener("click", () => {
+    conversor();
+});
+
+amountInput.addEventListener("keypress", (e) => {
+    if(e.key === "Enter") conversor();
+});
+
+conversor();
